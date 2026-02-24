117ac602000759d3063845ec9b4a70a5a2944ef3
Felix R. Benzan <felixbenz07@gmail.com>
2026-02-24 19:37:50 -0400

    Agregar tabla Estudiantes y crear página para listar estudiantes

--- Data/Migrations/20260224233731_NuevaTablaEstudiantes.Designer.cs
+++ Data/Migrations/20260224233731_NuevaTablaEstudiantes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExampleAcademia.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExampleAcademia.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224233731_NuevaTablaEstudiantes")]
+    partial class NuevaTablaEstudiantes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.1");
---
+++ Data/Migrations/20260224233731_NuevaTablaEstudiantes.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExampleAcademia.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevaTablaEstudiantes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Matricula = table.Column<string>(type: "varchar(9)", nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+        }
+    }
+}
