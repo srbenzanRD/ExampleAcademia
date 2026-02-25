7d33e8bc1c3753c1e64b0851a50ad5b2642e5057
Felix R. Benzan <felixbenz07@gmail.com>
2026-02-24 20:57:18 -0400

    Agregar modelo de vista EstudianteViewModel

---
+++ Data/ViewModels/EstudianteViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExampleAcademia.Data.ViewModels;
+
+public class EstudianteViewModel
+{
+    public int Id { get; set; }
+    public string Matricula { get; set; } = null!;
+    public string Nombre { get; set; } = null!;
+}
